0134743b82bee06857487ecd71d3cea73bae1042
Ricardo Manoel <rdomanoel@gmail.com>
2021-07-09 19:53:46 -0300

    dev: movies search with query

--- src/controller/client.controller.js
+++ src/controller/client.controller.js
@@ -14,33 +14,36 @@ exports.tmdb_movies = async (req, resp) => {
       'Authorization': `Bearer ${token}`
     }
   };
-
-  https.request(options, (res) => {
-    if (res.statusCode !== 200) {
-      console.error(`Did not get an OK from the server. Code: ${res.statusCode}`);
-      res.resume();
-      return;
-    }
-    let data = [];
-    res.on('data', (chunk) => {
-      data.push(chunk);
-    }).on('end', () => {
-      const parsed = JSON.parse(data);
-      const tmdbMovie = parsed.results.map((item) => {
-        let parsedData = new TMDBMovie(
-          item.id,
-          item.title,
-          item.genre_ids,
-          item.overview,
-          item.popularity,
-          item.vote_average,
-          item.release_date,
-        );
-        return parsedData
-      });
-      const tmdbMovieResults = new TMDBMovieResults (parsed.page, tmdbMovie);
-      resp.send(tmdbMovieResults);
-    })
-  }).end();
-  
+  try {
+    https.request(options, (res) => {
+      if (res.statusCode !== 200) {
+        console.error(`Did not get an OK from the server. Code: ${res.statusCode}`);
+        res.resume();
+        return;
+      }
+      let data = [];
+      res.on('data', (chunk) => {
+        data.push(chunk);
+      }).on('end', () => {
+        const parsed = JSON.parse(data);
+        const tmdbMovie = parsed.results.map((item) => {
+          let parsedData = new TMDBMovie(
+            item.id,
+            item.title,
+            item.genre_ids,
+            item.overview,
+            item.popularity,
+            item.vote_average,
+            item.release_date,
+          );
+          return parsedData
+        });
+        const tmdbMovieResults = new TMDBMovieResults (parsed.page, tmdbMovie);
+        resp.send(tmdbMovieResults);
+      })
+    }).end();
+  } catch (err) {
+    console.log('Error ocurred during API Client call', err)
+    resp.status(500).send(error);
+  }
 }
