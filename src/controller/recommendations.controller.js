c83325cc4eb9272189cdf73768fbd63bd2975d35
Ricardo Manoel <rdomanoel@gmail.com>
2021-08-28 18:14:49 -0300

    dev: recommendation put

--- src/controller/recommendations.controller.js
+++ src/controller/recommendations.controller.js
@@ -17,6 +17,7 @@ exports.recommendations_create = async (req, res) => {
     res.status(200);
     res.send('Recommendation Created');
   } catch (error) {
+    console.error(error);
     res.status(401);
     res.send('Error occurred during request');
   }
@@ -33,7 +34,7 @@ exports.recommendations_details = async (req, res) => {
 
 exports.recommendation_detail = async (req, res) => {
   try {
-    const recommendations = await Recommendations.findById(req.params.id);
+    const recommendations = await Recommendations.findOne({ id: req.params.id });
     if (!recommendations) res.status(404).send('No recommendation found');
     res.status(200).send(recommendations);
   } catch (error) {
@@ -54,9 +55,7 @@ exports.recommendations_details_from_user = async (req, res) => {
 exports.recommendation_update = async (req, res) => {
   try {
     const recommendation = await Recommendations.findByIdAndUpdate(
-      req.params.id, req.body, (err) => {
-        res.status(404).send('Impossible to update', err);
-      },
+      req.params.id, req.body,
     );
     await recommendation.save();
     res.status(200).send(recommendation);
@@ -66,7 +65,6 @@ exports.recommendation_update = async (req, res) => {
 };
 
 exports.recommendation_delete = async (req, res) => {
-  console.log(req.body);
   try {
     const recommendation = await Recommendations.findByIdAndDelete(req.body.id);
     if (!recommendation) res.status(404).send('No recommendation found');
--- src/routes/recommendation.route.js
+++ src/routes/recommendation.route.js
@@ -4,10 +4,10 @@ const recommendationsController = require('../controller/recommendations.control
 const recommendationsRouter = express.Router();
 
 recommendationsRouter.get('', recommendationsController.recommendations_details);
-recommendationsRouter.get('/:id', recommendationsController.recommendations_details);
+recommendationsRouter.get('/:id', recommendationsController.recommendation_detail);
 recommendationsRouter.get('/user/:id', recommendationsController.recommendations_details_from_user);
 recommendationsRouter.post('', recommendationsController.recommendations_create);
-recommendationsRouter.put('', recommendationsController.recommendation_update);
+recommendationsRouter.put('/:id', recommendationsController.recommendation_update);
 recommendationsRouter.delete('', recommendationsController.recommendation_delete);
 
 module.exports = recommendationsRouter;
